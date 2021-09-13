bcc1e943c73d61ddd28e48e19f8b98f046fa940e
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 19:59:56 -0400

    Update exercise.java

--- src/main/java/edu/ucf/exercise.java
+++ src/main/java/edu/ucf/exercise.java
@@ -9,6 +9,21 @@ import java.util.Scanner;
 
 public class exercise {
     public static void main(String[] args) {
-        
+        // create new scanner
+        Scanner input = new Scanner(System.in);
+
+        // read values
+        System.out.print("Enter the principle: ");
+        double principle = input.nextDouble();
+        System.out.print("Enter the rate of interest: ");
+        double rate = input.nextDouble();
+        System.out.print("Enter the number of years: ");
+        int years = input.nextInt();
+
+        // calculate worth
+        int worth = (int)Math.round(principle * (1 + (rate / 100 * years)));
+
+        // output results
+        System.out.print("After " + years + " years at " + rate + "%, the investment will be worth $" + worth + ".");
     }
 }
